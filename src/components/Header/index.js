fddd8ce6799ad7cb98df47eb03a55149c081938f
Victoire <victoirebaron@hotmail.fr>
2020-01-17 20:27:30 -0800

    update toolbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,15 +7,18 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from "@material-ui/icons/Email";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
-const useStyles = makeStyles({
-    grow: {
-        flexGrow: 1,
+const useStyles = makeStyles(theme => ({
+    icons: {
+        marginLeft: "auto",
+    },
+    appbar: {
+        backgroundColor: "#252834",
     },
     box: {
-        background: "rgb(37, 40, 52)",
+        background: "#252834",
         height: 500
-    }
-})
+    },
+}))
 
 
 export default function Header() {
@@ -23,30 +26,31 @@ export default function Header() {
 
     return (
         <div>
-            <AppBar position="static">
+            <AppBar position="static" className={classes.appbar}>
                 <Container>
-                    <Toolbar>
-                        <IconButton color="inherit">
-                            <GitHubIcon fontSize="large" aria-label="github" />
-                        </IconButton>
-                        <IconButton color="inherit">
-                            <LinkedInIcon fontSize="large" aria-label="linkedin" />
-                        </IconButton>
-                        <IconButton color="inherit">
-                            <EmailIcon fontSize="large" aria-label="email" />
-                        </IconButton>
+                    <Toolbar >
+                        <Box className={classes.icons} >
+                            <IconButton color="inherit">
+                                <GitHubIcon fontSize="large" aria-label="github" />
+                            </IconButton>
+                            <IconButton color="inherit">
+                                <LinkedInIcon fontSize="large" aria-label="linkedin" />
+                            </IconButton>
+                            <IconButton color="inherit">
+                                <EmailIcon fontSize="large" aria-label="email" />
+                            </IconButton>
+                        </Box>
                     </Toolbar>
                 </Container>
             </AppBar>
 
-            <Parallax
-                blur={10}
+            <Parallax  
                 bgImage={require('./assets/jumbotron.jpg')}
                 bgImageAlt="background"
                 strength={200}
             >
                 Victoire Baron
-            <div style={{ height: '200px' }} />
+            <div style={{ height: '450px' }} />
             </Parallax>
             <Box className={classes.box}>
 
