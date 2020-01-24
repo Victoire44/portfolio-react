95e512212f32f8de99168e0f5296e31aeb01c463
Victoire <victoirebaron@hotmail.fr>
2020-01-23 18:38:23 -0800

    add icons for skills

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { makeStyles, Box } from "@material-ui/core";
 import { Parallax } from "react-parallax";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, Typography, Link } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from "@material-ui/icons/Email";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import CardMedia from "@material-ui/core/cardMedia";
-
+import portrait from "./assets/portrait.jpg";
 
 const useStyles = makeStyles(theme => ({
-    icons: {
+    links: {
         marginLeft: "auto",
+    },
+    icons: {
+        fill: "#fff",
     },
     appbar: {
         backgroundColor: "#252834",
@@ -19,14 +21,29 @@ const useStyles = makeStyles(theme => ({
     box: {
         background: "#252834",
         height: 500,
-        color: "#fff",
+        color: "#fff"
     },
-    myname: {
+    name: {
         color: "#fff",
+        fontWeight: 400
     },
     title: {
         color: "#FA0063",
     },
+    padding: {
+        padding: 6,
+    },
+    parallax: {
+        height: 450,
+    },
+    portrait: {
+        height: 250,
+        borderRadius: "50%",
+        float: "left"
+    },
+    about: {
+        float: "left"
+    }
 }))
 
 
@@ -37,60 +54,69 @@ export default function Header() {
         <div>
             <AppBar position="static" className={classes.appbar}>
                 <Container>
-                    <Toolbar >
-                        <Box className={classes.icons} >
-                            <IconButton color="inherit">
-                                <GitHubIcon fontSize="large" aria-label="github" />
-                            </IconButton>
-                            <IconButton color="inherit">
-                                <LinkedInIcon fontSize="large" aria-label="linkedin" />
-                            </IconButton>
-                            <IconButton color="inherit">
-                                <EmailIcon fontSize="large" aria-label="email" />
-                            </IconButton>
+                    <Toolbar>
+                        <Box className={classes.links}>
+                            <Link href="https://github.com/Victoire44" target="_blank">
+                                <IconButton className={classes.padding}>
+                                    <GitHubIcon className={classes.icons} fontSize="large" aria-label="github" />
+                                </IconButton>
+                            </Link>
+                            <Link href="https://www.linkedin.com/in/victoirebaron/?locale=en_US" target="_blank">
+                                <IconButton className={classes.padding}>
+                                    <LinkedInIcon className={classes.icons} fontSize="large" aria-label="linkedin" />
+                                </IconButton>
+                            </Link>
+                            <Link href="mailto:victoirebaron@hotmail.fr" target="_blank" >
+                                <IconButton className={classes.padding}>
+                                    <EmailIcon className={classes.icons} fontSize="large" aria-label="email" />
+                                </IconButton>
+                            </Link>
                         </Box>
                     </Toolbar>
                 </Container>
             </AppBar>
 
             <Parallax
+                className={classes.parallax}
                 bgImage={require('./assets/jumbotron.jpg')}
                 bgImageAlt="background"
                 strength={900}
             >
                 <Box align="center">
-                    <Typography className={classes.myname} variant="h3" >Victoire Baron</Typography>
-                    <Typography className={classes.title} variant="h4" > Front end Web developer</Typography>
+                    <Typography variant="h2" className={classes.name}>Victoire Baron</Typography>
+                    <Typography className={classes.title} variant="h4"> Front End Web Developer</Typography>
                 </Box>
-                <div style={{ height: '350px' }} />
+
             </Parallax>
             <Box className={classes.box}>
                 <Container>
-                    <CardMedia
-                        image="./assets/portrait.jpg"
-                        title="portrait"
-                    />
-                    <Typography>
-                        Hello, I'm Victoire, an ambitious Full Stack Web Developer living in San Francisco who has
-                        transitioned over from being an expert in paintings.
+                    <div>
+                        <img
+                            src={portrait}
+                            className={classes.portrait}
+                            alt="portrait"
+                        />
+                        <Box className={classes.about}>
+                            <Typography>
+                                Hello, I'm Victoire, an ambitious Full Stack Web Developer living in San Francisco who has
+                                transitioned over from being an expert in paintings.
                 </Typography>
-                    <Typography>
-                        My background in Art History makes me think creatively with an artistic outlook. I think of
-                        the interface like a painting where I seek to create harmony and a good balance between the
-                        color and components.
+                            <Typography>
+                                My background in Art History makes me think creatively with an artistic outlook. I think of
+                                the interface like a painting where I seek to create harmony and a good balance between the
+                                color and components.
                 </Typography>
-                    <Typography>
-                        I enjoy creating and developing powerful products using HTML, CSS and React.js. I am equipped
-                            with the skills needed to create products that bring joy to users.
+                            <Typography>
+                                I enjoy creating and developing powerful products using HTML, CSS and React.js. I am equipped
+                                    with the skills needed to create products that bring joy to users.
                 </Typography>
-                    <Typography>
-                        Feel free to have a look on my work.
+                            <Typography>
+                                Feel free to have a look on my work.
                 </Typography>
-
+                        </Box>
+                    </div>
                 </Container>
             </Box>
         </div>
-
     )
-
 }
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, CardMedia } from '@material-ui/core/';
-import { Container, Typography } from "@material-ui/core/";
+import { Card, CardContent, CardHeader, CardMedia } from '@material-ui/core/';
+import { Container, Typography, Paper, Box } from "@material-ui/core/";
+import html from "./assets/html.svg";
+import css from "./assets/css.png";
+import bootstrap from "./assets/bootstrap.png";
+import materialize from "./assets/materialize.png";
+import material from "./assets/material.png";
+import javascript from "./assets/js.png";
+import react from "./assets/react.png";
+import jquery from "./assets/jquery.png";
+import node from "./assets/node.png";
+import mysql from "./assets/mysql.png";
+import mongo from "./assets/mongodb.png";
 
 
 const useStyles = makeStyles({
-
-
+    logos: {
+        height: 80,
+    }
 });
+
 export default function Skills() {
     const classes = useStyles();
 
@@ -15,25 +28,67 @@ export default function Skills() {
         <div>
             <Container>
                 <Typography variant="h3" align="center">Skills</Typography>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <CardMedia  
-                        image=".logos/css.png"
-                        />
+                <Paper elevation={2}>
+                    <CardHeader
+                        align="center"
+                        title="Front End"
+                    />
+                    <Box display="flex" flex="row">
+                        <img src={html} alt="HTML" className={classes.logos} />
                         <Typography className={classes.title}>
-                        HTML
-                     </Typography>
-
-                    </CardContent>
-                </Card>
-                <Card className={classes.card}>
-                    <CardContent>
+                            HTML
+                        </Typography>
+                        <img src={css} alt="CSS" className={classes.logos} />
                         <Typography className={classes.title}>
-                      
-                     </Typography>
+                            CSS
+                        </Typography>
+                        <img src={bootstrap} alt="Bootstrap" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            Boostrap
+                        </Typography>
+                        <img src={materialize} alt="Materialize" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            Materialize
+                        </Typography>
+                        <img src={material} alt="Material UI" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            Material UI
+                        </Typography>
+                        <img src={javascript} alt="Javascript" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            JavaScript
+                        </Typography>
+                        <img src={react} alt="React.js" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            React.js
+                        </Typography>
+                        <img src={jquery} alt="JQuery" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            JQuery
+                        </Typography>
+                    </Box>
+                </Paper>
 
-                    </CardContent>
-                </Card>
+                <Paper elevation={2}>
+                    <CardHeader
+                        align="center"
+                        title="Back End"
+                    />
+                    <Box display="flex" flex="row">
+                        <img src={node} alt="Node" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            Node
+                        </Typography>
+                        <img src={mysql} alt="MySql" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            MySql
+                        </Typography>
+                        <img src={mongo} alt="Mongo DB" className={classes.logos} />
+                        <Typography className={classes.title}>
+                            Mongo DB
+                        </Typography>
+                    </Box>
+                </Paper>
             </Container>
         </div>
     )
