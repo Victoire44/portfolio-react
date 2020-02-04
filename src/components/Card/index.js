ce7f892f24dbe46d2305c45475d9b59ca8bbbc40
Victoire <victoirebaron@hotmail.fr>
2020-02-03 23:20:15 -0800

    redesign of cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,44 +1,65 @@
 import React from 'react';
-import { makeStyles, Card, CardMedia, CardContent, CardActions, CardActionArea, IconButton, Link, Typography } from "@material-ui/core";
+import { makeStyles, IconButton, Link, Typography, Paper } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
+const radius = 1
 const useStyles = makeStyles(theme => ({
-    card: {
-        width: 345,
-        marginBottom: theme.spacing(8)
+    paper: {
+        height: 200,
+        position: "relative",
+        marginBottom: theme.spacing(8),
+        borderRadius: theme.spacing(radius)
     },
     media: {
-        height: 200,
+        height: "100%",
+        borderRadius: theme.spacing(radius)
     },
+    content: {
+        color: "#fff",
+        background: "#252834",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        borderRadius: theme.spacing(radius),
+        opacity: 0,
+        transition: "all 300ms ease-out",
+        "&:hover": {
+            opacity: 0.98
+        }
+    },
+    icons: {
+        color:"#fff"
+    },
+    info: {
+        padding: theme.spacing(2),
+    }
 }));
 
 export default function MediaCard(props) {
     const classes = useStyles();
 
     return (
-        <Card className={classes.card}>
-           <Link href={props.heroku} color="inherit" underline="none" target="_blank" rel="noopener"> 
-           <CardActionArea>
-                <CardMedia
-                    className={classes.media}
-                    image={props.image}
-                    title={props.alt}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {props.title}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {props.text}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            </Link>
-            <CardActions>
+        <Paper className={classes.paper} elevation={8}>
+            <img
+                className={classes.media}
+                src={props.image}
+                alt={props.alt}
+            />
+            <Link href={props.heroku} color="inherit" underline="none" target="_blank" rel="noopener" className={classes.content} align="center">
+                <div className={classes.info}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {props.title}
+                </Typography>
+                <Typography variant="body2" component="p">
+                    {props.text}
+                </Typography>
                 <Link href={props.github} target="_blank" rel="noopener">
                     <IconButton><GitHubIcon className={classes.icons} fontSize="large" aria-label="github" /></IconButton>
                 </Link>
-            </CardActions>
-        </Card>
+                </div>
+            </Link>
+        </Paper>
     );
 }
